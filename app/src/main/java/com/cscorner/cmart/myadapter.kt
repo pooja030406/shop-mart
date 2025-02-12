31fa60ababb94c735afa377af1775e3174654c80
siddharth2004Shukla <siddharth.shuklapti@gmail.com>
2025-02-12 16:31:06 +0530

    fixed the recycler view repetion of items.

--- app/src/main/java/com/cscorner/cmart/myadapter.kt
+++ app/src/main/java/com/cscorner/cmart/myadapter.kt
@@ -16,14 +16,18 @@ class myadapter(val context: MainActivity2, val arr: List<Product>): RecyclerVie
     }
 
     override fun getItemCount(): Int {
-        return arr.size
+        return arr.size/2;
     }
 
     override fun onBindViewHolder(holder: viewholder, position: Int) {
-        holder.binding.textView8.text = arr.get(position).title
-        holder.binding.textView9.text = arr.get(position).title
-        Picasso.get().load(arr.get(position).thumbnail).into(holder.binding.imageView9)
-        Picasso.get().load(arr.get(position).thumbnail).into(holder.binding.imageView10)
+
+            holder.binding.textView9.text = arr.get(arr.size/2+position).title
+            holder.binding.textView8.text = arr.get(position).title
+
+
+            Picasso.get().load(arr.get(position).thumbnail).into(holder.binding.imageView9)
+            Picasso.get().load(arr.get(arr.size/2+position).thumbnail).into(holder.binding.imageView10)
+
     }
 
 }
